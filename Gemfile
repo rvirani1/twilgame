00c412d02261e0358995f9e856cf70cdb5c06422
Riaz Virani <riaz.n.virani@gmail.com>
2014-07-29 16:37:31 -0400

    added test to newcall

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 ruby "2.1.2"
 gem 'sinatra', '1.1.0'
+gem 'httparty'
+gem 'twilio-ruby'
--- web.rb
+++ web.rb
@@ -1,4 +1,26 @@
+require 'rubygems'
 require 'sinatra'
+require 'twilio-ruby'
+enable :sessions
+
+get '/newcall' do
+  #session[:twilioID] = params[]
+  Twilio::TwiML::Response.new do |r|
+    r.Say 'Hello Monkey'
+  end.text
+end
+
+
+get '/foo' do
+  session[:message] = 'Hello World!'
+  redirect to('/bar')
+end
+
+get '/bar' do
+  session[:message]   # => 'Hello World!'
+end
+
 get '/' do
   "Learning Ruby on Heroku"
 end
+
