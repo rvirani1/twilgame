3dc012286922ef866231d54205d4fc4c40b9c324
Riaz Virani <riaz.n.virani@gmail.com>
2014-07-29 18:15:37 -0400

    set hardcode parameter

--- web.rb
+++ web.rb
@@ -13,7 +13,7 @@ auth_token = '8466dfddd99695943abcc64161e9db05'
 
 post '/newcall' do
   session[:callSid] = params[:CallSid]
-  session[:rightNum] = Random.rand(9)
+  # session[:rightNum] = Random.rand(9)
   Twilio::TwiML::Response.new do |r|
     r.Gather :timeout => 30, :action => '/guess' do |g|
       g.Say 'Welcome to the number guessing game. Please guess a number from one to ten'
@@ -23,17 +23,16 @@ post '/newcall' do
 end
 
 post '/guess' do
-  if session[:rightNum] == params[Digits]
-    Twilio::TwiML::Response.new do |r|
-      r.Say 'You guessed the wrong number. Call again'
-    end.text
-  else
+  if params[Digits] == 1
     Twilio::TwiML::Response.new do |r|
       r.Say 'You guessed the right number. You be baller dude'
       r.Play "http://com.twilio.music.rock.s3.amazonaws.com/jlbrock44_-_Apologize_Guitar_Deep_Fried.mp3"
     end.text
+  else
+    Twilio::TwiML::Response.new do |r|
+      r.Say 'You guessed the wrong number. Call again'
+    end.text
   end
-
 end
 
 
